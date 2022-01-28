e707bc74f73b9bc0f9f355ab9450e37f1929ba0c
garlife <garlife@gmail.com>
2022-01-28 23:12:45 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
+export default class App extends React.Component {
+ constructor(props){
+   super(props);
+   this.state = {
+     name: '',
+     text: ''
+   }
+ }
+
+ handlerInput = (e) => {
+   let value = e.target.value;
+   this.setState({
+    text: value
+   })
+ }
+
+ handlerAssignName = () => {
+   if( this.state.text === ''){
+     return(
+      alert('Сначала нужно ввести имя')
+     )
+   }
+   else{
+    this.setState({
+      name: this.state.text
+    })
+   }
+ }
+
+  render(){
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Привет{this.state.name === ('') ? '. Укажи свое имя' :  ', ' + this.state.name}!</h1>
+      <input type="text" value={this.state.text} onChange={this.handlerInput}></input>
+      <button onClick={this.handlerAssignName}>Указать имя</button>
     </div>
   );
+}
 }
